aeafae0225ebdd4fdadd7736c364a577e5a5e7b8
Gery Mont <gerymont@gmail.com>
2017-08-12 12:27:59 -0300

    Creadas 3 tarjetas, una grande y dos chicas una al lado de la otra

--- pages/cards.vue
+++ pages/cards.vue
@@ -1,7 +1,21 @@
 <template>
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
-      <v-card>
+       <v-card class="po">
+        <v-card-media src="http://www.po.org.ar/uploads/noticias//thumb_ver_nota_zoom/30346.jpg" height="200px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Partido Obrero</h3>
+            <div>Sitio web del Partido Obrero de la Argentina</div>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text" tag="a" href="http://www.po.org.ar" target="_blank">Web</v-btn>
+          <v-btn flat class="orange--text" nuxt-link to="/">Home</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card class="liverpool">
         <v-card-media src="https://pbs.twimg.com/profile_images/857505840626118656/A1V_u7-u.jpg" height="200px">
         </v-card-media>
         <v-card-title primary-title>
@@ -12,6 +26,20 @@
         </v-card-title>
         <v-card-actions>
           <v-btn flat class="orange--text" tag="a" href="http://www.liverpoolfc.com/welcome-to-liverpool-fc" target="_blank">Web</v-btn>
+          <v-btn flat class="orange--text" nuxt-link to="/prueba">Prueba</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card class="literautas">
+        <v-card-media src="https://1.bp.blogspot.com/-O_EdJex7aUM/V584OJrRUkI/AAAAAAAAAYw/GbpUz-3YDUAOZjCOFERhg4TsY9lN7pv9wCLcB/s1600/literautas.JPG" height="200px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Literautas</h3>
+            <div>Sitio web de Literautas</div>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text" tag="a" href="http://www.literautas.com/" target="_blank">Web</v-btn>
           <v-btn flat class="orange--text" nuxt-link to="/prueba">Prueba</v-btn>
         </v-card-actions>
       </v-card>
@@ -33,6 +61,24 @@ export default {
 
 h3{
   color:#d3171e;
+}
+
+.liverpool{
+  display:inline-block;
+  margin-right:2%;
+  width:49%;
+}
+
+.literautas{
+  display:inline-block;
+  /*margin-right:15px;*/
+  /*height:150px;*/
+  width:49%;
+}
+
+.po{
+/*display:inline-block;*/
+margin-bottom:15px;
 }
 
 </style>
